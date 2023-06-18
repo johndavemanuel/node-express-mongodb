fb6014b4d933fd77dae152e15d036c1cb0576431
John Dave Manuel <jdmanuel242@gmail.com>
2023-06-18 21:58:34 +0800

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: "mongodb://0.0.0.0:27017/bezkoder_db"
+//   url: "mongodb://0.0.0.0:27017/bezkoder_db"
+url: "mongodb+srv://dave:ZuIO9y4ERAthXSqc@cluster0.z4uxy.mongodb.net/?retryWrites=true&w=majority"
 };
